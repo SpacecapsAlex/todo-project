aad1c80b5368b938aa63f099b51fcdf880480150
SpacecapsAlex <SpacecapsAlex@hotmail.com>
2025-04-08 22:04:32 +0300

    add start page

--- lmsystem-front/src/App.tsx
+++ lmsystem-front/src/App.tsx
@@ -5,13 +5,15 @@ import { LoginPage } from "./pages/LoginPage/LoginPage.tsx";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage.tsx";
 import { NotFound } from "./pages/NotFound/NotFound.tsx";
 import { Layout } from "./Layout";
+import { Dashboard } from './pages/Dashboard/Dashboard.tsx';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<TodoMainPage />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/todo" element={<TodoMainPage />} />
           <Route path="/login" element={<LoginPage /> } />
           <Route path="register" element={<RegisterPage /> } />
           <Route path="*" element={<NotFound />} />
